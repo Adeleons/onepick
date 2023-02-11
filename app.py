53bd7b8992ed10f360e7e1f9ab8b207b814de122
Adeleons <ad_chameleon@naver.com>
2023-02-11 14:42:26 +0900

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import random
 from flask import Flask, render_template, request, jsonify
 
 from pymongo import MongoClient
@@ -9,61 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('main_page.html')
+    return render_template('index.html')
 
+@app.route("/lunch", methods=['GET'])
+def lunch_post():
+   city_receive = request.form.get('city')
+   category_receive = request.form.get('category')
 
-@app.route("/lunch", methods=["GET"])
-def web_lunch_get():
-    lunch_list = list(db.lunch.find({}, {'_id': False}))
-    return jsonify({"lunchs": lunch_list})
+   lunch_list = list(db.lunch.find({'city': {'$in': [city_receive]}}, {'category': {'$in': [category_receive]}}))
+
+   for d in lunch_list:
+       return render_template('index.html', s_restaurant=d['restaurant'], s_represent=d['represent'],
+                              s_food=d['food'])
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5100, debug=True)
-
-
-
-# from flask import Flask, render_template, request, jsonify
-# import random
-
-# from pymongo import MongoClient
-# client = MongoClient('mongodb+srv://kimseyoung:onepick@cluster0.fzbyzfc.mongodb.net/Cluster0?retryWrites=true&w=majority')
-# db = client.onepick
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#    return render_template('main_page.html')
-
-# @app.route('/lunch', methods=['POST'])
-# def web_lunch_get():
-#    city_receive = request.form.get('city_receive')
-#    category_receive = request.form.get('category_receive')
-#    represent_receive = request.form.get('represent_receive')
-#    restaurant_receive = request.form.get('restaurant_receive')
-
-#    doc = {
-#       "city" : city_receive,
-#       "category" : category_receive,
-#       "represent" : represent_receive,
-#       "restaurant" : restaurant_receive
-      
-#    }
-
-   
-#    # db.gangnam.find({'city': {'$in': [city_receive]}},{'class': {'$in': [class_receive]}} )
-#    for d in db.lunch.find({'city': {'$in': [city_receive]}},{'category': {'$in': [category_receive]}}):
-#    # for d in db.lunch.find(doc({'city': {'$in': [city_receive]}},{'category': {'$in': [category_receive]}})):
-#       print(d['restaurant'], d['represent'], d['food'])
-
-#    return render_template('real_menu_result.html', s_restaurant = d['restaurant'], s_represent = d['represent'], s_food = d['food'])
-
-
-# # @app.route("/gangnam", methods=["GET"])
-# # def web_gangnam_get():
-#     # comment_list = list(db.homework.find({},{'_id':False}))
-#     # choice = random.choice(comment_list)
-#     # return jsonify({'comments': choice})
-
-
-# if __name__ == '__main__':
-#    app.run('0.0.0.0',port=5100,debug=True)
